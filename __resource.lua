6ef9eb8423c9181ae3deb07d38c43affa04520ba
Brad <58879069+xlimeTigerx@users.noreply.github.com>
2020-04-15 16:10:27 -0600

    Changed everything to Staff prefixes

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,4 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_script "cl_emergencyblips.lua"
-server_script "sv_emergencyblips.lua"
+client_script "cl_staff.lua"
+server_script "sv_staff.lua"
